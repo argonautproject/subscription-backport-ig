bc3e1319ab27ad0f6be7260cbac29464a66960b2
Gino Canessa <ginocanessa@gmail.com>
2020-05-22 16:08:57 -0500

    Add docs for GH Pages (temp)

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
-rd input output /s/q
+rd input output docs /s/q
 call build-vars.cmd
 call sushi fsh -o .
 copy fsh\ig-data\ig.bak ig.ini
 %JAVAEXE% -Xmx2G -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini
+ren output docs
